504927b7bd0bed2a16305ded310fa274a74bfe44
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2025-11-09 00:59:28 +0600

    Add Dashboard component with PoolsGrid

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PoolsGrid from "../components/PoolsGrid";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <PoolsGrid />
+    </div>
+  );
+}
